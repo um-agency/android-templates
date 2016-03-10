5acf2960b2f529f7b72c2f7e528d889b8b4f6da8
Gavriil Sitnikov <zuzuk666@gmail.com>
2016-03-11 02:46:24 +0300

    first class added

---
+++ src/main/java/ru/touchin/templates/TouchinApp.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (c) 2016 Touch Instinct
+ *
+ *  This file is part of RoboSwag library.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package ru.touchin.templates;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.multidex.MultiDex;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by Gavriil Sitnikov on 10/03/16.
+ */
+public abstract class TouchinApp extends Application {
+
+    protected abstract boolean isDebug();
+
+    @Override
+    protected void attachBaseContext(@NonNull final Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (isDebug()) {
+            //TODO
+        } else {
+            final Crashlytics crashlytics = new Crashlytics();
+            Fabric.with(this, crashlytics);
+        }
+    }
+
+}
