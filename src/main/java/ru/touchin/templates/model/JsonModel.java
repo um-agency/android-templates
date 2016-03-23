00b808d0e1b235db5d9383bba90e5f21a926ea26
Gavriil Sitnikov <zuzuk666@gmail.com>
2016-03-23 04:44:13 +0300

    api model renamed

--- src/main/java/ru/touchin/templates/model/JsonModel.java
+++ src/main/java/ru/touchin/templates/model/JsonModel.java
@@ -7,34 +7,28 @@ import com.google.api.client.util.Data;
 
 import java.util.List;
 
+import ru.touchin.roboswag.core.data.Model;
+
 /**
  * Created by Gavriil Sitnikov on 13/11/2015.
  */
-public class ApiModel {
+public abstract class JsonModel extends Model {
 
-    public static boolean isNullOrEmpty(@Nullable final Object object) {
+    protected static boolean isNullOrEmpty(@Nullable final Object object) {
         return object == null || Data.isNull(object);
     }
 
     @Nullable
-    public static <T> T makeNullable(@Nullable final T data) {
+    protected static <T> T makeNullable(@Nullable final T data) {
         return !isNullOrEmpty(data) ? data : null;
     }
 
-    public static boolean isListValid(@NonNull final List list) {
+    protected static boolean isListValid(@NonNull final List list) {
         for (final Object item : list) {
-            if (item == null || ((item instanceof ApiModel) && !((ApiModel) item).isValid())) {
+            if (item == null || ((item instanceof JsonModel) && !((JsonModel) item).isValid())) {
                 return false;
             }
         }
-        return true;
-    }
-
-    public void validate() {
-        //do nothing
-    }
-
-    public boolean isValid() {
         return true;
     }
 
