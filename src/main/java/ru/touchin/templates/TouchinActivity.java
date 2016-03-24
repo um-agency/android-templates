da4093f734289028c5426da5a9ae611d6027889a
Gavriil Sitnikov <zuzuk666@gmail.com>
2016-03-24 20:07:45 +0300

    logic renaming

--- src/main/java/ru/touchin/templates/TouchinActivity.java
+++ src/main/java/ru/touchin/templates/TouchinActivity.java
@@ -1,12 +1,13 @@
 package ru.touchin.templates;
 
 import ru.touchin.roboswag.components.navigation.ViewControllerActivity;
+import ru.touchin.roboswag.components.utils.Logic;
 
 /**
  * Created by Gavriil Sitnikov on 11/03/16.
  * TODO: description
  */
-public abstract class TouchinActivity<TLogicBridge> extends ViewControllerActivity<TLogicBridge> {
+public abstract class TouchinActivity<TLogic extends Logic> extends ViewControllerActivity<TLogic> {
 
     protected TouchinActivity() {
         super();
