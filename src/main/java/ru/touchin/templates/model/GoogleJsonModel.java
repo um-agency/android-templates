9ba8a2b59854a891c2a812c1ccc6da59185806f8
Gavriil Sitnikov <zuzuk666@gmail.com>
2016-04-17 18:29:30 +0300

    json model a bit fixed

---
+++ src/main/java/ru/touchin/templates/model/GoogleJsonModel.java
@@ -0,0 +1,36 @@
+package ru.touchin.templates.model;
+
+import android.support.annotation.Nullable;
+
+import com.google.api.client.util.Data;
+
+import ru.touchin.roboswag.core.data.Model;
+import ru.touchin.roboswag.core.data.exceptions.ValidationException;
+
+/**
+ * Created by Gavriil Sitnikov on 13/11/2015.
+ */
+public abstract class GoogleJsonModel extends Model {
+
+    protected static boolean isNullOrEmpty(@Nullable final Object object) {
+        return object == null || Data.isNull(object);
+    }
+
+    @Nullable
+    protected static <T> T makeNullable(@Nullable final T data) {
+        return !isNullOrEmpty(data) ? data : null;
+    }
+
+    protected static void validateNonNull(@Nullable final Object object) throws ValidationException {
+        if (isNullOrEmpty(object)) {
+            throw new ValidationException("Not nullable object is null or empty");
+        }
+    }
+
+    protected static void validateNonEmpty(@Nullable final Object object) throws ValidationException {
+        if (object == null) {
+            throw new ValidationException("Not empty object is empty");
+        }
+    }
+
+}
